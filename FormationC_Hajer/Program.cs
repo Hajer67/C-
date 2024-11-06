0f35fe3f985cbdf9ab44d6f9e6072a330266e987
User <User@LAPTOP-H6GDXD3>
2024-11-06 09:29:26 +0100

    Premier commit

---
+++ FormationC_Hajer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationC_Hajer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

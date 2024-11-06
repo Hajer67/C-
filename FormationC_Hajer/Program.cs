0884b3670c61e1ad6ed074d3e565009b93610a7f
User <User@LAPTOP-H6GDXD3>
2024-11-06 17:35:56 +0100

    Exercice 2

--- FormationC_Hajer/Program.cs
+++ FormationC_Hajer/Program.cs
@@ -8,28 +8,51 @@ namespace FormationC_Hajer
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Exercice 1 - Opérations élémentaires ");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Opérations de base : ");
             BasicOperation(3, 4, '+');
             BasicOperation(3, 0, '/');
             BasicOperation(3, 4, 'L');
 
             Console.WriteLine(" ");
 
+            Console.WriteLine("Division entière : ");
             IntegerDivision(12, 4);
             IntegerDivision(13, 4);
             IntegerDivision(12, 0);
 
             Console.WriteLine(" ");
 
+            Console.WriteLine("Puissance entière : ");
             Pow(5, 2);
             Pow(5, -2);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Exercice 2 - Horloge parlante ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            GoodDay(1);
+            GoodDay(10);
+            GoodDay(12);
+            GoodDay(15);
+            GoodDay(20);
+            GoodDay(24);
 
             Console.ReadKey();
         }
 
+        // EXERCICE I - OPERATIONS ELEMENTAIRES
+
         static void BasicOperation(int a, int b, char operateur)
         {
+            // Opérations de base : 
             int r;
             //char operateur = { +, -, *,/};
             switch (operateur)
@@ -71,6 +94,7 @@ namespace FormationC_Hajer
 
         static void IntegerDivision(int a, int b)
         {
+            // Division entière :
 
             if (b == 0)
             {
@@ -101,6 +125,8 @@ namespace FormationC_Hajer
 
         static void Pow(int a, int b)
         {
+            // Puissance entière :
+
             int résultat;
 
             if (b < 0)
@@ -110,7 +136,7 @@ namespace FormationC_Hajer
 
             else
             {
-                résultat = a ^ b;
+                résultat = (int)Math.Pow(a, b);
 
 
                 {
@@ -119,8 +145,53 @@ namespace FormationC_Hajer
 
 
             }
+
         }
+
+
+        // EXERCICE II - HORLOGE PARLANTE
+
+       
+        static void GoodDay(int heure)
+        {
+            if (0 <= heure && heure < 6) 
+            {
+                Console.WriteLine($"Il est {heure} H, Merveilleuse nuit !");
+            }
+
+            else if (6 <= heure && heure < 12) 
+            {
+                Console.WriteLine($"Il est {heure} H, Bonne matinée !");
+            }
+
+            else if (heure == 12)
+            {
+                Console.WriteLine($"Il est {heure} H, Bon appétit !");
+            }
+
+            else if (13 <= heure && heure <= 18)
+            {
+                Console.WriteLine($"Il est {heure} H, Profitez de votre après-midi !");
+            }
+
+            else if (heure > 18 && heure < 24)
+            {
+                Console.WriteLine($"Il est {heure} H, Passez une bonne soirée !");
+            }
+
+            else 
+            {
+                Console.WriteLine($"Il n'est pas {heure} H, Apprenez à compter svp !");
+            }
+
+        }
+
+
+
+
+
     }
+
         
 }
 

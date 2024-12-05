8bf4e7ff51d9a1a7b0a970f9711f4eb0a2b2059c
alexandrebordes <Alex.bds.fr@gmail.com>
2024-12-05 17:16:31 +0100

    Partie 2 suite

--- Projet partie 2/Projet partie 2/CompteBancaire.cs
+++ Projet partie 2/Projet partie 2/CompteBancaire.cs
@@ -9,20 +9,25 @@ namespace Projet_partie_2
     internal class CompteBancaire
     {
         public uint IdentifiantCompte { get; private set; }
-
         public decimal Solde { get; private set; }
+        public DateTime DateRésiliation { get; set; }
+        public DateTime DateCréation { get; set; }
+        public uint NombreTransactionsMax { get; set; }
 
-        private const decimal _maxRetrait = 2000;
-        private const DateTime.Equals(_dateCréation,_dateRésiliation)
-        private DateTime _dateCréation;
-        private DateTime _dateRésiliation;
+        private const decimal _maxRetrait = 1000;
+        private TimeSpan _periodeRetraitMax = new TimeSpan(7, 0, 0, 0);
         private List<decimal> _historiqueRetrait;
+        private Dictionary<decimal, DateTime> _historiqueHebdomadaire;
 
-        public CompteBancaire(uint identifiantCompte, decimal solde)
+        public CompteBancaire(uint identifiantCompte, decimal solde, uint nombreTransactionsMax, DateTime dateCréation)
         {
             IdentifiantCompte = identifiantCompte;
             Solde = solde;
             _historiqueRetrait = new List<decimal>();
+            NombreTransactionsMax = nombreTransactionsMax;
+            DateCréation = dateCréation;
+            DateRésiliation = DateTime.MaxValue;
+            _historiqueHebdomadaire= new Dictionary<decimal, DateTime>();
         }
 
         private decimal SommeRetraits(decimal transaction)
@@ -35,6 +40,12 @@ namespace Projet_partie_2
             }
             return sommeRetraits;
         }
+
+      /*  private decimal SommeRetraitsHebdomadaire(decimal transaction) 
+        {
+            decimal sommeRetraits 
+        }
+      */
 
         private void NouveauRetrait(decimal montant)
         {
@@ -49,18 +60,18 @@ namespace Projet_partie_2
             }
         }
 
-        public bool IsDepositValid(decimal montant)
+        public bool IsDepositValid(Transactions transactions)
         {
-            if (montant <= 0)
+            if (transactions.Montant <= 0 || transactions.DateEffet >= DateCréation || transactions.DateEffet < DateRésiliation)
             {
                 return false;
             }
             return true;
         }
 
-        public bool IsWithdrawalValid(decimal montant)
+        public bool IsWithdrawalValid(Transactions transactions)
         {
-            if (montant <= 0 || Solde < montant || SommeRetraits(montant) >= _maxRetrait)
+            if (transactions.Montant <= 0 || Solde < transactions.Montant || transactions.DateEffet >= DateCréation || transactions.DateEffet < DateRésiliation )
             {
                 return false;
             }
---
+++ Projet partie 2/Projet partie 2/Gestionnaire.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_partie_2
+{
+    internal class Gestionnaire
+    {
+        public uint Identifiant { get; private set; }
+        public string Type { get; private set; }
+        public int NombreTransactionsMax { get; private set; }
+        public decimal FraisGestion { get; set; }
+    }
+}

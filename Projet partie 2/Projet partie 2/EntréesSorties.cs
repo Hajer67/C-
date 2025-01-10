8a13528a5fea4842aae272427be977c8ef8a4ce4
Formation <Formation@DESKTOP-AN89IF0>
2025-01-10 16:57:51 +0100

    partie 2 suite

--- Projet partie 2/Projet partie 2/EntréesSorties.cs
+++ Projet partie 2/Projet partie 2/EntréesSorties.cs
@@ -10,14 +10,13 @@ namespace Projet_partie_2
 {
     internal class EntréesSorties
     {
-        public const string fichierComptes = @"C:\Users\Formation\Desktop\C#\PartieI\Comptes_1.txt";
-        public static Dictionary<uint, CompteBancaire> FichierComptes()
+        public static Dictionary <uint, CompteBancaire> FichierComptes(string fichierComptes)
         {
             if (!File.Exists(fichierComptes))
             {
                 throw new FileNotFoundException("Fichier d'entrée non trouvé", fichierComptes);
             }
-            Dictionary<uint, CompteBancaire> comptes = new Dictionary<uint, CompteBancaire>();
+            Dictionary <uint, CompteBancaire> comptes = new Dictionary <uint, CompteBancaire>();
 
             using (FileStream fsComptes = File.OpenRead(fichierComptes))
             using (StreamReader lectureComptes = new StreamReader(fsComptes))
@@ -81,21 +80,21 @@ namespace Projet_partie_2
                     }
                     else
                     {
-                        comptes.Add(identifiant, new CompteBancaire(identifiant, solde, 10, date));
+                        comptes.Add(identifiant, new CompteBancaire(identifiant, solde, nombreTransactionsMax, date));
                     }
                 }
                 return comptes;
             }
         }
 
-        public static Dictionary<uint, Transactions> FichierTransactions(string fichierTransactions)
+        public static Dictionary <uint, Transactions> FichierTransactions(string fichierTransactions)
         {
             if (!File.Exists(fichierTransactions))
             {
                 throw new FileNotFoundException("Fichier d'entrée non trouvé", fichierTransactions);
             }
 
-            Dictionary<uint, Transactions> transactions = new Dictionary<uint, Transactions>();
+            Dictionary <uint, Transactions> transactions = new Dictionary <uint, Transactions>();
 
             using (FileStream fsTransactions = File.OpenRead(fichierTransactions))
             using (StreamReader lectureTransactions = new StreamReader(fsTransactions))
@@ -137,7 +136,7 @@ namespace Projet_partie_2
                         Console.WriteLine($"Transaction {lignesTransactions[0]} : L'identifiant ne peut pas être égal à zéro.");
                         continue;
                     }
-                     if (!DateTime.TryParseExact(lignesTransactions[1],"d",fr,DateTimeStyles.None, out dateEffet))
+                    if (!DateTime.TryParseExact(lignesTransactions[1],"d",fr,DateTimeStyles.None, out dateEffet))
                     {
                         Console.WriteLine($"Transaction {lignesTransactions[0]} : La date renseignée est incorrecte.");
                         continue;
@@ -173,13 +172,14 @@ namespace Projet_partie_2
                 throw new FileNotFoundException("Fichier d'entrée non trouvé", fichierGestionnaires);
             }
 
-            Dictionary<uint, Gestionnaire> gestionnaires = new Dictionary<uint, Gestionnaire>();
+            Dictionary <uint, Gestionnaire> gestionnaires = new Dictionary <uint, Gestionnaire>();
 
             using (FileStream fsGestionnaires = File.OpenRead(fichierGestionnaires))
             using (StreamReader lectureGestionnaires = new StreamReader(fsGestionnaires))
             {
                 while (!lectureGestionnaires.EndOfStream)
                 {
+                    List <string> gestionnaireValide = new List <string> ("Particulier","Entreprise");
                     string[] lignesGestionnaires = lectureGestionnaires.ReadLine().Split(';');
                    
                     if (lignesGestionnaires.Length != 3)
@@ -187,21 +187,19 @@ namespace Projet_partie_2
                         Console.WriteLine($"Gestionnaire {lignesGestionnaires[0]} : Le format de la ligne n'est pas correct : {lignesGestionnaires.Length}");
                         continue;
                     }
+
                     if (lignesGestionnaires[1] == string.Empty || !string.TryParse(lignesGestionnaires[1], out string type))
                     {
                         Console.WriteLine($"Gestionnaire {lignesGestionnaires[0]} : Le gestionnaire demandé ne possède pas de type.");
                         continue;
                     }
-                    /*else if (!decimal.TryParse(lignesTransactions[2], out montant))
-                    {
-                        Console.WriteLine($"Transaction {lignesTransactions[0]} : Un charactère non numérique dans le montant de la transaction a été détecté.");
+
+                    if (!gestionnaireValide.Find(x => x.Equals(lignesGestionnaires[1]))
+	                {
+                        Console.WriteLine($"Gestionnaire {lignesGestionnaires[0]} : Le gestionnaire ne possède pas de type valide.");
                         continue;
-                    }
-                    if (montant < 0)
-                    {
-                        Console.WriteLine($"Transaction {lignesTransactions[0]} : Le montant de la transaction ne peut pas être négatif.");
-                        continue;
-                    }*/
+	                }
+	               
                     if (!uint.TryParse(lignesGestionnaires[0], out uint identifiant))
                     {
                         Console.WriteLine($"Gestionnaire {lignesGestionnaires[0]} : L'identifiant du gestionnaire demandé n'existe pas.");
@@ -212,21 +210,13 @@ namespace Projet_partie_2
                         Console.WriteLine($"Gestionnaire {lignesGestionnaires[0]} : L'identifiant d'un gestionnaire ne peut pas être égal à zéro.");
                         continue;
                     }
-                   /*  if (!DateTime.TryParseExact(lignesTransactions[1],"d",fr,DateTimeStyles.None, out dateEffet))
+
+                    if (!uint.TryParse(lignesTransactions[2], out uint nombreTransactions))
                     {
-                        Console.WriteLine($"Transaction {lignesTransactions[0]} : La date renseignée est incorrecte.");
+                        Console.WriteLine($"Transaction {lignesTransactions[0]} : Le nombre de transactions n'est pas valide");
                         continue;
                     }
-                    if (!uint.TryParse(lignesTransactions[3], out uint expéditeur))
-                    {
-                        Console.WriteLine($"Transaction {lignesTransactions[0]} : L'expéditeur demandé n'existe pas.");
-                        continue;
-                    }
-                    if (!uint.TryParse(lignesTransactions[4], out uint destintaire))
-                    {
-                        Console.WriteLine($"Transaction {lignesTransactions[0]} : Le destinataire demandé n'existe pas.");
-                        continue;
-                    }*/
+
                     if (gestionnaires.ContainsKey(identifiant))
                     {
                         continue;
@@ -239,9 +229,9 @@ namespace Projet_partie_2
                 return gestionnaires; 
             }
 
-        }
+         }
 
-        public static void fichierStatusTransactions(Dictionary<uint, Transactions> statutsTransactions)
+        public static void fichierStatusTransactions(Dictionary <uint, Transactions> statutsTransactions)
         {
             using (FileStream fsOut = File.Create(@"C:\Users\Formation\Desktop\C#\PartieI\Statuts_b.txt"))
             using (StreamWriter ecritureTransactions = new StreamWriter(fsOut))
--- Projet partie 2/Projet partie 2/Gestionnaire.cs
+++ Projet partie 2/Projet partie 2/Gestionnaire.cs
@@ -8,14 +8,23 @@ namespace Projet_partie_2
 {
     internal class Gestionnaire
     {
-        public uint Identifiant { get; private set; }
+        public uint IdentifiantGestionnaire { get; private set; }
         public string Type { get; private set; }
         public int NombreTransactionsMax { get; private set; }
         public decimal FraisGestion { get; set; }
     
+        private readonly Dictionary <uint, CompteBancaire> _comptes;
+
+        public Gestionnaire(uint identifiantGestionnaire, string type, uint nombreTransactionsMax)
+        {
+            IdentifiantGestionnaire = identifiantGestionnaire;
+            Type = type;
+            NombreTransactionsMax = nombreTransactionsMax;
+            FraisGestion = 0;
+            _comptes = new Dictionary<uint, CompteBancaire>();
+        }
     
-    
-    
+        
     
     
     }
